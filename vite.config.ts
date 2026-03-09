0f4f00a5c0f2c2e26241efab6fabf8ed5eadff45
Ansalidre <cacheman25@gmail.com>
2026-03-09 01:02:17 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "base: "/ERASTI-ARCHITECTURE-STUDIO/",
+  base: "/ERASTI-ARCHITECTURE-STUDIO/",
   server: {
     host: "::",
     port: 8080,
